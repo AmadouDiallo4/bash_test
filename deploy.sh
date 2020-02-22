76400d47f0b3932c8ad4017cb045824507aae60e
Vagrant Default User <vagrant@control>
2020-02-22 20:28:02 +0000

    add script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+###############################################################
+#
+# Description: deploiement à la volée de conteneur docker
+#
+# Auteur: Amadou
+# date: 22/02/2020
+# 
+################################################################
+
+echo "
+
+Options:
+  - --create:   lancer des conteneurs
+  - --drop:     supprimer es conteneurs par deploy.sh
+  - --infos:    caractéristiques des conteneurs (ip, non, user, ...)
+  - --start:    redémarrage des conteneurs
+  - --ansible:  déploiement arborescence ansible
+
+"
