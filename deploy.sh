b23c021da9731f35141245a111611ba61fbe2ce5
AmadouDiallo4 <diallobis2011@gmail.com>
2020-03-16 12:06:48 +0000

    [DEV]: maj

--- deploy.sh
+++ deploy.sh
@@ -19,12 +19,20 @@ if [ "$1" == "--create" ];then
 	echo ""
 	echo "notre option est --create"
 	echo ""
+	
+	nb_machine=1
+	[ "$2" != "" ] && nb_machine=$2
+
+	docker run -tid --name $USER-alpine alpine:latest
+	echo "j'ai crée ${nb_machine}"
 
 #si option --drop
 elif [ "$1" == "--drop" ];then
 	echo ""
 	echo "notre option est --drop"
 	echo ""
+	
+	docker rm -f $USER-alpine
 
 #si option --infos
 elif [ "$1" == "--infos" ];then
